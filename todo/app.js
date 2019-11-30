46b86f866caeb60d21403504dc986ee40041fecc
andrew <drewdoolittle@gmail.com>
2019-11-30 10:42:16 -0800

    styling and transition features

--- todo/app.js
+++ todo/app.js
@@ -1,14 +1,22 @@
 //$(document).ready() {
-    $("li").click(function () {
-        $(this).toggleClass("completed");
+    $("ul").on("click", "li", function () {      //use on click for future/potential events
+        $(this).toggleClass("completed");       //must be bound to an existing node when DOM is first fired
     });
 
-    $("span").click(function(event){
-        alert("clicked on span")
+    $("ul").on("click", "span", function(event){
+        $(this).parent().fadeOut(500, function(){ //this refers to span
+            $(this).remove();                     //this refers to li (its parent)
+        })
         event.stopPropagation();
     });
 
-
+    $("input[type='text'").keypress(function(event){
+        if(event.which === 13) {
+            var todoText = $(this).val();
+            $(this).val("")
+            $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+        }
+    })
 
 
 
