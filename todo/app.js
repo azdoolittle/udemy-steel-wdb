4bba1a24d34b364cd8c1ea72195cee84a99ea358
andrew <drewdoolittle@gmail.com>
2019-12-02 21:53:29 -0800

    removed doc.ready function, spacing

--- todo/app.js
+++ todo/app.js
@@ -1,31 +1,18 @@
-//$(document).ready() {
-    $("ul").on("click", "li", function () {      //use on click for future/potential events
-        $(this).toggleClass("completed");       //must be bound to an existing node when DOM is first fired
+$("ul").on("click", "li", function () {      //use on click for future/potential events
+    $(this).toggleClass("completed");       //must be bound to an existing node when DOM is first fired
+});
+
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){ //this refers to span
+        $(this).remove();                     //this refers to li (its parent)
     });
+    event.stopPropagation();
+});
 
-    $("ul").on("click", "span", function(event){
-        $(this).parent().fadeOut(500, function(){ //this refers to span
-            $(this).remove();                     //this refers to li (its parent)
-        })
-        event.stopPropagation();
-    });
-
-    $("input[type='text'").keypress(function(event){
-        if(event.which === 13) {
-            var todoText = $(this).val();
-            $(this).val("")
-            $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
-        }
-    })
-
-
-
-
-
-
-
-
-
-
-
-//};
+$("input[type='text'").keypress(function(event){
+    if(event.which === 13) {
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+    }
+});
